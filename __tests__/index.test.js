6f98fd0db05077c64a1f4c64b4c0b9ff14f15a4e
Annetjie <afreakanist@gmail.com>
2020-05-22 17:21:20 +0600

    test and fixtures fixes (correct path joining, filename extensions removed + trim() again)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,9 +10,9 @@ const argSets = outputFormats.map((outputFormat) => (
 )).flat(); // ?!
 
 test.each(argSets)('show difference between %s files in %s format', (fileFormat, outputFormat) => {
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
   const before = getFixturePath(`before.${fileFormat}`);
   const after = getFixturePath(`after.${fileFormat}`);
-  const correctOutput = fs.readFileSync(getFixturePath(outputFormat), 'utf-8');
+  const correctOutput = fs.readFileSync(getFixturePath(outputFormat), 'utf-8').trim();
   expect(gendiff(before, after, outputFormat)).toEqual(correctOutput);
 });
